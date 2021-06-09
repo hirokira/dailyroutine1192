3c0ad278a40057088dc99aa15955b4df096b0901
hiro.K <tak_kawa011616@yahoo.co.jp>
2021-06-10 07:31:26 +0900

    dailyroutine

--- src/main/java/DailyRoutineApp/app/controller/LoginController.java
+++ src/main/java/DailyRoutineApp/app/controller/LoginController.java
@@ -9,14 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import DailyRoutineApp.app.service.UserAccountService;
-
 @Controller
 public class LoginController {
 
-
-	@Autowired
-	private UserAccountService sv;
 
 	@Autowired
 	private HttpSession session;
