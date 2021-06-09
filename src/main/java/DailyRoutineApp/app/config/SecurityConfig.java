ea1b0ad4320af08be84612b65714c2992fa9a7d2
hiro.K <tak_kawa011616@yahoo.co.jp>
2021-06-10 00:37:02 +0900

    dailyroutine

--- src/main/java/DailyRoutineApp/app/config/SecurityConfig.java
+++ src/main/java/DailyRoutineApp/app/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		web.ignoring().antMatchers("/img/neko.png",
 									"/css/**",
 									"/js/script.js",
-									"template/account/acNew.html"
+									"..template/account/acNew.html"
 									);
 	}
 
