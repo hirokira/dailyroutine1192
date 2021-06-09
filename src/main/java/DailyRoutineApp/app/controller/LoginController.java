6d3853f80fe9a90d8668948644d5ddeaef2b0d9c
hiro.K <tak_kawa011616@yahoo.co.jp>
2021-06-10 06:56:55 +0900

    dailyroutine

--- src/main/java/DailyRoutineApp/app/controller/LoginController.java
+++ src/main/java/DailyRoutineApp/app/controller/LoginController.java
@@ -43,7 +43,7 @@ public class LoginController {
 
 	@RequestMapping(value="/hello",method=RequestMethod.GET)
 	public ModelAndView hello(ModelAndView mav) {
-		mav.setViewName("/account/hello");
+		mav.setViewName("account/hello");
 		return mav;
 	}
 
