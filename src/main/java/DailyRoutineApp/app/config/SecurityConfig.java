f11891ef463cfd3217c751915c95a7b96beacea2
hiro.K <tak_kawa011616@yahoo.co.jp>
2021-06-10 00:16:34 +0900

    dailyroutine

--- src/main/java/DailyRoutineApp/app/config/SecurityConfig.java
+++ src/main/java/DailyRoutineApp/app/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,6 +20,46 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UserDetailsService userDetailsService;
 
+
+    /**
+     * 認可設定を無視するリクエストを設定
+     * 静的リソース(image,javascript,css)を認可処理の対象から除外する
+     */
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/img/neko.png",
+									"/css/**",
+									"/js/script.js",
+									"template/acNew.html"
+									);
+	}
+
+    /**
+     * 認証・認可の情報を設定する
+     * 画面遷移のURL・パラメータを取得するname属性の値を設定
+     * SpringSecurityのconfigureメソッドをオーバーライドしています。
+     */
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+        http
+        .authorizeRequests()
+        	.antMatchers("/account/new").permitAll()//test用(ユーザーリスト)※終わったら消す。
+            .anyRequest().authenticated()//上記以外は全て認証無しの場合アクセス不許可
+            .and()
+        .formLogin()
+            .loginPage("/login") //ログインページはコントローラを経由しないのでViewNameとの紐付けが必要
+            .defaultSuccessUrl("/routine/top", true) //認証が成功した際に遷移するURL
+            .failureUrl("/login-error") //認証が失敗した際に遷移するURL
+            .permitAll() //どのユーザでも接続できる。
+            .and()
+        .logout()
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login")
+            .permitAll();
+}
+
+	/*
 	@Override
 	protected void configure(HttpSecurity web)throws Exception{
 
@@ -28,6 +69,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         .anyRequest().authenticated();
 		web.logout().logoutSuccessUrl("/login").permitAll();
 	}
+	*/
 
 	/**
      * 認証時に利用するデータソースを定義する設定メソッド
