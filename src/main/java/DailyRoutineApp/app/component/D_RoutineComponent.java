08550a2ebf58704196a2cbfb552d47057dc7e3ae
hiro.K <tak_kawa011616@yahoo.co.jp>
2021-06-26 11:49:35 +0900

    不要なＤＩ削除

--- src/main/java/DailyRoutineApp/app/component/D_RoutineComponent.java
+++ src/main/java/DailyRoutineApp/app/component/D_RoutineComponent.java
@@ -1,17 +1,11 @@
 package DailyRoutineApp.app.component;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import DailyRoutineApp.app.entity.D_Routine;
-import DailyRoutineApp.app.service.D_RoutineService;
 
 @Component
 public class D_RoutineComponent {
-
-
-	@Autowired
-	private D_RoutineService service;
 
 	/*
 	 * いいねが押された時、１加算して返却する処理
