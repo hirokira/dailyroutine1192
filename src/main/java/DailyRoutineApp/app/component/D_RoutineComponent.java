5fe67e2d0cd9dc0f8731779e282b747e8049f612
hiro.K <tak_kawa011616@yahoo.co.jp>
2021-06-26 11:53:13 +0900

    こうしん

--- src/main/java/DailyRoutineApp/app/component/D_RoutineComponent.java
+++ src/main/java/DailyRoutineApp/app/component/D_RoutineComponent.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Component;
 
 import DailyRoutineApp.app.entity.D_Routine;
 
+
+
 @Component
 public class D_RoutineComponent {
 
 	/*
-	 * いいねが押された時、１加算して返却する処理
+	 * いいねが押された時、１加算して返却する処理a
 	 */
 	public Integer nicePntAdd(D_Routine routine) {
 		return routine.getNicepnt()+1;//---routineオブジェクトのnicePntを１加算する
